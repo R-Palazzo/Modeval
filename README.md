f63a91fc332e07065213cbec1103ed40d3751c40
R-Palazzo <romain.palazzo@datacebo.com>
2023-01-30 12:38:23 +0100

    automate testing 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Modeval
+
+![Tests](https://github.com/mCodingLLC/SlapThatLikeButton-TestingStarterProject/actions/workflows/tests.yml/badge.svg)
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import find_packages, setup
+
+tests_require = [
+    'pytest>=3.4.2',
+    'pytest-cov>=2.6.0',
+    'pytest-rerunfailures>10',
+    'jupyter>=1.0.0,<2',
+    'rundoc>=0.4.3,<0.5',
+]
 setup(
     name='modeval',
     packages=find_packages(include=['modeval']),
@@ -8,6 +16,6 @@ setup(
     license='MIT',
     install_requires=[],
     setup_requires=['pytest-runner'],
-    tests_require=['pytest==7.1.0'],
+    tests_require= tests_require,
     test_suite='tests',
 )
